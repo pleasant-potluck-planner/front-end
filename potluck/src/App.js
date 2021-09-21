8dc33f1fcb2cbe6d679f3705b71b053d38cb3d56
cc <votaryofturms@icloud.com>
2021-09-21 16:54:23 -0400

    form

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import AddEvent from './Components/CreateEvent'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [potlucks, setPotlucks] = useState([])
+
+  const onSubmit = (evnt,) => {
+    evnt.preventDefault();
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Potluck Planner</h1>
+      <AddEvent onSubmit={onSubmit} />
     </div>
   );
 }
